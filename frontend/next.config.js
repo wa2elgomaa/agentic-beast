e6e36b90c6ebf65a178f5d2b66daca63248367c1
Wael Gomaa <wg_gomaa@hotmail.com>
2026-05-06 11:02:05 +0400

    change host BE

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_API_PREFIX: process.env.NEXT_PUBLIC_API_PREFIX || '/api/v1',
   },
   webpack: (config, { isServer }) => {
